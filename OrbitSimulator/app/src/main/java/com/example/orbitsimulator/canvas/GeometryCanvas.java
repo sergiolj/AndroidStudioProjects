585146a94ac732372421e02319f40992c3fac30a
Sergio Lopes Junior <sergiolj@gmail.com>
2025-11-03 14:34:06 -0300

    layout iniciado

---
+++ OrbitSimulator/app/src/main/java/com/example/orbitsimulator/canvas/GeometryCanvas.java
@@ -0,0 +1,18 @@
+package com.example.orbitsimulator.canvas;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class GeometryCanvas extends View {
+
+    public GeometryCanvas(Context context) {
+        super(context);
+    }
+
+    public GeometryCanvas(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
